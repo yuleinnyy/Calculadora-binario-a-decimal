84d9b7f0f46cbd55d7873d2a0e111a1fcf49f16b
ortizyuleinny@gmail.com <ortizyuleinny@gmail.com>
2020-11-23 17:22:49 -0400

    Agregar archivos de proyecto.

---
+++ Calculadora binario a decimal/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_binario_a_decimal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            string bin = "";
+            bool v = true;
+            Console.Write("Escribe un numero binario: ");
+            bin = Console.ReadLine();
+            for (int x = bin.Length - 1, Y = 0; x >= 0; x--, Y++)
+            {
+                if (bin[x] == '0' || bin[x] == '1')
+                    n += (int)(int.Parse(bin[x].ToString()) * Math.Pow(2, Y));
+                else
+                {
+                    Console.Write("El numero no es binario");
+                    v = false;
+                    break;
+                }
+            }
+            if (v)
+                Console.Write("El numero decimal es: {0}", n);
+            Console.ReadKey();
+
+        }
+    }
+}
